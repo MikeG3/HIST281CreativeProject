df7982696c11548a70c79541710fe5ac7d2ae451
MikeG3 <harveymichaelgarner@hotmail.com>
2022-04-15 11:49:25 -0400

    Corona Map

--- title.js
+++ title.js
@@ -2,7 +2,7 @@ var red = 0, green = 0, blue = 0;
 
 const letters = ["t1","t2","t3","t4","t5","t6","t7","t8","t9","t10","t11","t12","t13","t14","t15","t16","t17","t18","t19","t20","t21","t22"];
 
-function updateColors(){
+function updateTitleColors(){
     red += 1;
     blue += 2;
     green += 3;
@@ -12,6 +12,7 @@ function updateColors(){
 }//close function update colors
 
 function animateTitle(){
+    updateTitleColors()
     for (let i = 0 ; i < letters.length ; i++ ){
         let color = 'rgb(' + red + ',' + blue + ',' + green + ')';
         document.getElementById(letters[i]).style.color = color;
